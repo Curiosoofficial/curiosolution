563e98511b7f8f9d349de88e95c514f9dcf0bd2c
Luka Stefanovic <lukastefanoviccurioso@gmail.com>
2024-05-20 23:27:51 +0200

    trying designs

--- components/UnserService/webdesign/TitleSection.tsx
+++ components/UnserService/webdesign/TitleSection.tsx
@@ -7,8 +7,8 @@ import React from 'react'
 const TitleSection = () => {
   return (
     <section className="flex flex-col justify-center items-center py-16 px-72">
-        <h2 className="h2-bold font-radwave pt-16">WEBDESIGN</h2>
-        <div className="grid grid-cols-2 gap-7 pt-12">
+        <h2 className="h2-bold font-radwave pt-16 z-10">WEBDESIGN</h2>
+        <div className="grid grid-cols-2 gap-7 pt-12 z-10">
         {infoCards.map((card) => (
         <InfoCard
           key={card.title}
@@ -18,8 +18,12 @@ const TitleSection = () => {
         />
       ))}
         </div>
-        <p className="py-16">Fordern Sie eine kostenlose Projektbewertung an und erhalten Sie einen Monat kostenlos</p>
+        <p className="py-16 z-10">Fordern Sie eine kostenlose Projektbewertung an und erhalten Sie einen Monat kostenlos</p>
         <Button />
+
+        <div className="circle1 absolute top-[0px] left-[200px] transform w-[800px] h-[800px] z-0 rounded-full"></div>
+        
+        
     </section>
   )
 }
--- components/ui/Button.tsx
+++ components/ui/Button.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Button = () => {
   return (
-    <button className="px-5 py-2 rounded-md bg-accent-yellow text-black font-bold transition duration-200 hover:bg-white hover:text-black border-2 border-transparent hover:border-accent-yellow">
+    <button className="z-10 px-5 py-2 rounded-md bg-accent-yellow text-black font-bold transition duration-200 hover:bg-white hover:text-black border-2 border-transparent hover:border-accent-yellow">
       ANFRAGEN
     </button>
   );
